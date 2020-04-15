9f1194395d799a62e076e68db93d91e32e860c18
Mateusz Tocha <tocha.mateusz@gmail.com>
2020-04-15 19:40:21 +0200

    Media done

--- src/media/media.ts
+++ src/media/media.ts
@@ -4,18 +4,19 @@
 * Media
 * ------------------
 *
-* Goal: At Przeprogramowani.pl we use multiple platforms at once - make sure that our favorite one works!
+* Goal: At Przeprogramowani.pl we use multiple platforms at once -
+* make sure that our favorite one works!
 * 
 * Hint: https://www.typescriptlang.org/docs/handbook/enums.html
 */
 
 enum Media {
-    Internet,
-    Press,
-    Radio
+    Internet='Internet',
+    Press='Press',
+    Radio='Radio'
 }
 
-const przeprogramowani: Media = Media.Press;
+const przeprogramowani= Media.Internet.toString();
 
 /* Do not modify tests */
 
