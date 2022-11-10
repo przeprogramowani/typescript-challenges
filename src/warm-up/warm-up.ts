12f6ef6a503b304d8e3fadfb44e40268b849367e
Michał Barczyński <michal.barczynski97@gmail.com>
2022-11-10 11:46:20 +0100

    warm-up done

--- src/warm-up/warm-up.ts
+++ src/warm-up/warm-up.ts
@@ -9,9 +9,9 @@
 * Hint: https://www.typescriptlang.org/docs/handbook/basic-types.html
 */
 
-const age: string = 80;
-const firstName: number = 'Przemek';
-const todos: Map = [
+const age: number = 80;
+const firstName: string = 'Przemek';
+const todos: Array<object> = [
     { todo: 'Learn TypeScript' },
     { todo: 'Subscribe Przeprogramowani', priority: 1},
     { todo: 'Sign up for newsletter', priority: 1}
